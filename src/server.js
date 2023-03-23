d011f88679620d512bed3e8bf7caf75f2f0494d5
wahyudi.prasatia <wahyudi.prasatia@koltiva.com>
2023-03-23 12:53:49 +0700

    add auth

--- src/server.js
+++ src/server.js
@@ -1,11 +1,22 @@
 const Hapi = require('@hapi/hapi');
 require('dotenv').config();
+const Jwt = require('@hapi/jwt');
 const notes = require('./api/notes');
 const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
+const CatchEvent = require('./extensions/index');
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
+const authentications = require('./api/authentications');
+const AuthenticationsService = require('./services/postgres/AuthenticationsService');
+const TokenManager = require('./tokenize/TokenManager');
+const AuthenticationsValidator = require('./validator/authentications');
 
 const init = async () => {
   const notesService = new NotesService();
+  const usersService = new UsersService();
+  const authenticationsService = new AuthenticationsService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -17,14 +28,55 @@ const init = async () => {
     },
   });
 
-  await server.register({
+  await server.register([
+    {
+      plugin: Jwt,
+    },
+  ]);
+
+  server.auth.strategy('notesapp_jwt', 'jwt', {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
+  });
+
+  await server.register([{
     plugin: notes,
     options: {
       service: notesService,
       validator: NotesValidator,
     },
 
-  });
+  }, {
+    plugin: users,
+    options: {
+      service: usersService,
+      validator: UsersValidator,
+    },
+
+  },
+  {
+    plugin: authentications,
+    options: {
+      authenticationsService,
+      usersService,
+      tokenManager: TokenManager,
+      validator: AuthenticationsValidator,
+    },
+  },
+  ]);
+
+  await server.ext('onPreResponse', CatchEvent);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
